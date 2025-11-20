29da29fd70e843b6c0c3024fd38059d900b69ede
Jvicto-dev <joaovicto12112001@gmail.com>
2025-11-20 15:12:18 -0300

    tabelas que iremos usar no projeto

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,8 @@
 import type { FastifyInstance } from 'fastify'
+import { db as knex } from '../database.js'
 
-
-export async function usersRoutes(app: FastifyInstance){
-    app.get('/', (request, replay)=>{
+export async function usersRoutes(app: FastifyInstance) {
+    app.get('/', (request, replay) => {
         console.log("Deu certo")
     })
 }
